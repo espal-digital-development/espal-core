542e3438db891d4ec16d1d4bce698ce9dd661b5b
Erwin <espaldd@icloud.com>
2020-06-24 22:33:29 +0200

    Init

--- stores/page/translation_synthesized_test.go
+++ stores/page/translation_synthesized_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/espal-digital-development/espal-core/stores/page"
 )
+
 func TestTranslationTable(t *testing.T) {
 	translation := page.NewTranslationEntity()
 	if translation.TableName() == "" {
